37f90e3b67ed7b11ed1d7dcdcc65d5d80ff4985e
kara2703 <kabraanoushka02@gmail.com>
2022-07-07 22:03:00 -0700

    changes to main page name

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import SvgWave from '@components/SvgWave';
 import backgroundImage from '../public/assets/crested-butte-2016-07-14.jpg';
 import meImage from '../public/assets/kyle-pfromer.jpg';
 
-export interface HomeProps {}
+export interface HomeProps { }
 
 const Home: React.FC<HomeProps> = () => {
   return (
@@ -44,7 +44,7 @@ const Home: React.FC<HomeProps> = () => {
 
             <h1 className="text-6xl text-primary-500 font-bold">Hi!</h1>
 
-            <h2 className="text-5xl text-white font-bold">I'm Kyle Pfromer</h2>
+            <h2 className="text-5xl text-white font-bold">I'm Anoushka Kabrar</h2>
 
             <h3 className="text-2xl text-white">
               and I'm a <span className="font-bold text-2xl">Software Engineer</span>
