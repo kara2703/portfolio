5abf935dbc73ef8d407e3cd4aff8b5897ea15e4e
kara2703 <kabraanoushka02@gmail.com>
2022-07-08 20:50:20 -0700

    change to linkedin info.ts

--- configs/info.ts
+++ configs/info.ts
@@ -2,7 +2,7 @@ const info = {
   baseUrl: 'https://anoushkakabra.com',
   resume: 'https://drive.google.com/file/d/1KUxBSQwj87LBoHmsuRAyPgfN6F6oo5kR/view',
   github: 'https://github.com/kpfromer',
-  linkedin: 'https://www.linkedin.com/in/kyle-pfromer',
+  linkedin: 'https://www.linkedin.com/in/anoushka-k-2a450118b/',
   stackoverflow: 'https://stackoverflow.com/users/3448490/kyle-pfromer',
 };
 
