154b5dbd5e7699e803b3bbaa35421ab231cc4f11
kara2703 <kabraanoushka02@gmail.com>
2022-07-08 20:59:26 -0700

    change to info.ts

--- configs/info.ts
+++ configs/info.ts
@@ -1,9 +1,9 @@
 const info = {
   baseUrl: 'https://anoushkakabra.com',
-  resume: 'https://drive.google.com/file/d/1KUxBSQwj87LBoHmsuRAyPgfN6F6oo5kR/view',
-  github: 'https://github.com/kpfromer',
+  resume: 'https://drive.google.com/file/d/1GQ6XtpPuvtzjaYl9zLmV1nTUWxjyqqzP/view?usp=sharing',
+  github: 'https://github.com/kara2703',
   linkedin: 'https://www.linkedin.com/in/anoushka-k-2a450118b/',
-  stackoverflow: 'https://stackoverflow.com/users/3448490/kyle-pfromer',
+  stackoverflow: 'https://stackoverflow.com',
 };
 
 export default info;
