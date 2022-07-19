2950a93d65474dc7d0b043f05d9b52a636252765
kara2703 <kabraanoushka02@gmail.com>
2022-07-18 23:00:33 -0700

    no blog4

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ import { Heading } from '@components/Heading';
 import Page from '@components/Page';
 import React from 'react';
 
-export interface ContactPageProps {}
+export interface ContactPageProps { }
 
 const ContactPage: React.FC<ContactPageProps> = () => {
   return (
@@ -13,11 +13,9 @@ const ContactPage: React.FC<ContactPageProps> = () => {
 
       <p>
         Hi! My name is <span className="text-bold">Anoushka Rakesh Kabra</span>. I am very curious
-        person who enjoys learning about computer science, economics and finance. I am currently a computer science student at
-
-        <a className="text-yellow-600 underline" 
-        href="http://cis.cornell.edu/cornell-computing-information-science">
-          
+        person who enjoys learning about computer science, economics and finance.
+        I am currently a computer science student at
+        <a className="text-yellow-600 underline" href="http://cis.cornell.edu/cornell-computing-information-science">
           Cornell University, College of Engineering
         </a>
         . When not programming I am reading, dancing, or working out.
