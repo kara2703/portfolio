c768dd472dcc571cfb309b59ba195e2d320ad7d3
kara2703 <kabraanoushka02@gmail.com>
2022-09-26 18:41:25 -0400

    changing resume1

--- configs/info.ts
+++ configs/info.ts
@@ -1,6 +1,6 @@
 const info = {
   baseUrl: 'https://anoushkakabra.com',
-  resume: 'https://drive.google.com/file/d/1LZV9Q3xC-wKaIxxR1MABVjm5Kz-_8HPv/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1wqdQWCMV2f39G7PUITln5eZC-8shraxd/view?usp=sharing',
   github: 'https://github.com/kara2703',
   linkedin: 'https://www.linkedin.com/in/anoushka-k-2a450118b/',
   instagram: 'https://www.instagram.com/xnoushkx/',
